12cd13e92cadd8d423fbccb1f69785d38601dbd2
jw <70766701+wldn143@users.noreply.github.com>
2021-03-19 21:01:45 +0900

    BOJ 10769번

---
+++ 10769.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+string s;
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(0);
+	int happy = 0, sad = 0;
+	getline(cin, s);
+	for (int i = 0; i < s.size() - 2; i++) {
+		if (s[i] == ':' && s[i + 1] == '-')
+		{
+			if (s[i + 2] == ')')
+				happy++;
+			else
+				sad++;
+		}
+	}
+	if (happy == 0 && sad == 0)
+		cout << "none\n";
+	else if (happy == sad)
+		cout << "unsure\n";
+	else if (happy > sad)
+		cout << "happy\n";
+	else if (happy < sad)
+		cout << "sad\n";
+
+}
+//for (int i = 0; i < s.length() - 2; i++) {
+//	string temp = s.substr(i, 3);
+//	if (temp == ":-)")
+//	{
+//		happy++;
+//	}
+//	if(temp == ":-(")
+//		sad++;
+//	}
+//}
+
+
