bcf06c394644af919b542449e51f905561d0fbd5
jw <70766701+wldn143@users.noreply.github.com>
2021-03-19 20:56:37 +0900

    BOJ 4459번

---
+++ 4459.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int q, r, question;
+string rules[60];
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(0);
+
+	cin >> q;
+	cin.ignore();
+	for (int i = 1; i <= q; i++) {
+		getline(cin, rules[i]);
+	}
+
+	cin >> r;
+	for (int i = 1; i <= r; i += 1) {
+		cin >> question;
+		if (question<1 || question>q) {
+			cout << "Rule " << question << ": No such rule\n";
+		}
+		else {
+			cout << "Rule " << question << ": " << rules[question] << '\n';
+		}
+	}
+
+}
