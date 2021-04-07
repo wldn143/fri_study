f9e0b3f33e8a270a50f2dd7b691be085b4a30f05
jw <70766701+wldn143@users.noreply.github.com>
2021-04-08 00:20:17 +0900

    BOJ 10174번

---
+++ 10174.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int n;
+	
+	vector<string> v;
+	
+	cin >> n;
+	cin.ignore();
+
+	for (int i = 0; i < n; i++)
+	{
+		string s;
+		getline(cin,s);
+		//왜 한 문장을 덜 받아? getline이 문제 cin.ignore()이 필수
+		
+		bool flag = true;
+		
+		for (int i = 0; i < s.length() / 2; i++)
+		{
+			if (toupper(s[i]) != toupper(s[s.length() - i - 1]))
+			{
+				flag = false;
+				break;
+			}
+		}
+		if (flag)
+			cout << "Yes\n";
+		else
+			cout << "No\n";
+  }
+}
