262ac2d26735fc24a1bb3417e2d7d5187447037c
jw <70766701+wldn143@users.noreply.github.com>
2021-04-20 22:43:47 +0900

    BOJ 1402번

---
+++ 1402.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+int main(void)
+
+{
+
+        int test_case;
+
+        cin >> test_case;
+
+ 
+
+        for (int i = 0; i < test_case; i++)
+
+        {
+
+                 int multiple, add;
+
+                 cin >> multiple >> add;
+
+                 cout << "yes" << endl;
+
+        }
+
+}
