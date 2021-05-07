e64c7e65a492b4e49864686e00df53b6b06523e1
jw <70766701+wldn143@users.noreply.github.com>
2021-05-08 00:09:12 +0900

    Update 2750.cpp

--- 2750.cpp
+++ 2750.cpp
@@ -23,3 +23,25 @@ int main(void){
         cout << arr[l] << endl;
     }
 }
+
+//sort함수 이용
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int N;
+	int arr[1000];
+	
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arr[i];
+	}
+	sort(arr, arr + N);
+	for (int i = 0; i < N; i++)
+	{
+		cout << arr[i] << "\n";
+	}
+}
